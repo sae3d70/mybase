72e630849df8cf4e23164f455d077c1f191afef2
sae3d70 <140123564+sae3d70@users.noreply.github.com>
2023-07-21 02:42:48 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0e61605d2263f204d08759df9793aa922fe3b2f3";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
